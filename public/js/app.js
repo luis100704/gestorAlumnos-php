38dbd682378b3b2dec56540e59da404b18783792
Luis Fernando Eugenio Castillo <luiseugenio808@gmail.com>
2026-01-26 20:34:55 +0100

    AJAX: cargar y mostrar alumnos en la tabla

--- public/js/app.js
+++ public/js/app.js
@@ -1,4 +1,42 @@
 $(document).ready(function () {
-    console.log('JS conectado correctamente');
+
+    cargarAlumnos();
+  
+    function cargarAlumnos() {
+      $.ajax({
+        url: '../api/getAlumnos.php',
+        type: 'GET',
+        dataType: 'json',
+        success: function (respuesta) {
+  
+          if (!respuesta.success) {
+            alert('Error al cargar alumnos');
+            return;
+          }
+  
+          let filas = '';
+  
+          respuesta.data.forEach(function (alumno) {
+            filas += `
+              <tr>
+                <td>${alumno.nombre}</td>
+                <td>${alumno.email}</td>
+                <td>${alumno.curso}</td>
+                <td>
+                  <button class="btn btn-sm btn-warning">Editar</button>
+                  <button class="btn btn-sm btn-danger">Eliminar</button>
+                </td>
+              </tr>
+            `;
+          });
+  
+          $('#tablaAlumnos').html(filas);
+        },
+        error: function () {
+          alert('Error en la petición AJAX');
+        }
+      });
+    }
+  
   });
   
